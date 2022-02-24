0153a02ee09919c0472f5c49f7932d787759dc12
Cezar <cezarguimaraes@gmail.com>
2022-02-23 21:27:53 -0300

    Rota getProductsById implementada)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const express = require('express');
+const productsController = require('./controllers/productsController');
 require('dotenv').config();
+
+const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsController);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
