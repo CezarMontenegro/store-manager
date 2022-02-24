508e9f085222a500430b07d137ec6f95645eab7d
Cezar <cezarguimaraes@gmail.com>
2022-02-24 00:20:43 -0300

    requisito 1 teste 3

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const [result] = await connection.execute(
-   'SELECT * FROM products',
+   'SELECT * FROM StoreManager.products',
   );
  
   return result;
@@ -10,7 +10,7 @@ const getAll = async () => {
 
 const getById = async (id) => {
   const [result] = await connection.execute(
-    'SELECT * FROM products WHERE id = ?', [id],
+    'SELECT * FROM StoreManager.products WHERE id = ?', [id],
   );
 
   if (result.length === 0) {
